a207a4f19e28543ba3ccbac92f7f30779b5122bd
Jaisal Shah <57360897+jaisal1311@users.noreply.github.com>
2022-05-29 18:01:37 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-
+def solve(arr, k):
+    d = {}
+    for i in range(len(arr)):
+        if k - arr[i] in d:
+            return True
+        d[arr[i]] = True
+    return False
+print(solve([10, 15, 3, 7], 17))
